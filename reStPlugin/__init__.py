c7fdedd29ecb6d8047a423cbd1da6a39b60cd4c9
François Poirotte <clicky@erebot.net>
2011-11-16 00:48:06 +0100

    Add support for some of Pygments' options.
    - Register "code", "code-block", "sourcecode" & "sourcecode-block" as
      aliases to the same directive.
    - Rewrite the "pygments_directive" function as a class (Pygments).
    - Create a new formatter each time. It takes more time to format the
      document because of that, BUT :
    - Add support for some of Pygment's formatting options :
      * linenos (as a flag)
      * hl_lines
      * linenostart
      * linenostep
      * linenospecial
      * nobackground (as a flag)
      * anchorlinenos (as a flag)
      * noclasses (as a flag)

--- reStPlugin/__init__.py
+++ reStPlugin/__init__.py
@@ -210,12 +210,6 @@ class restPlugin(gedit.Plugin):
         text = doc.get_text(start, end)
         html = publish_parts(text, writer_name="html")["html_body"]
 
-        ## Sortie
-        sortie = '\n'.join([START_HTML,html,END_HTML])
-        fs = open('sortie.html','w')
-        fs.write(sortie)
-        fs.close()
-
         p = windowdata["bottom_panel"].get_placement()
 
         html_doc = windowdata["html_doc"]
