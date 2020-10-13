c8f4ad98c08479b1ab9a78166767c01d4fa57068
Jan Kanis <jan.code@jankanis.nl>
2020-10-13 16:22:06 +0200

    cleanup & refactor

--- reST/restructuredtext.py
+++ reST/restructuredtext.py
@@ -26,6 +26,24 @@ from docutils.core import publish_parts
 from gi.repository import Gtk, WebKit2, GLib
 from os.path import abspath, dirname, join
 import threading
+from enum import Enum
+import sys
+
+class State(Enum):
+    NON_REST = 1
+    REST = 2
+    SELECTION = 3
+    EXIT = 4
+
+DEBUG = True
+def debug(*args):
+    if DEBUG:
+        print(*args, file=sys.stderr)
+
+
+class PreviewArgs:
+    def __init__(self, text=None, html=None, html_type=None, parent_window=None):
+        self.text, self.html, self.html_type, self.parent_window = text, html, html_type, parent_window
 
 
 class RestructuredtextHtmlPanel(Gtk.ScrolledWindow):
@@ -52,15 +70,19 @@ class RestructuredtextHtmlPanel(Gtk.ScrolledWindow):
         Gtk.ScrolledWindow.__init__(self)
 
         self.lock = threading.Lock()
-        self.alive = True
         self.event = threading.Event()
         self.worker = threading.Thread(target=self.rest_parser_thread, name="gedit-reST-plugin worker")
         self.worker.start()
-        self.show_reST = False
 
-        # To restore scroll positions after redrawing the preview, and across selecting text
+        # state is the state we want to be in (and in which the text area is). It is updated when the text
+        # area updates.
+        self.state = State.NON_REST
+
+        # To restore scroll positions after redrawing the preview, and across selecting text. Contains the most recent
+        # position when we were showing a (non-selection) reST preview.
         self.last_position = None
-        self.last_position_active = False
+        # scroll_position_valid indicates if the preview panel currently contains a reST (non-selection) preview
+        self.scroll_position_valid = False
 
         module_dir = dirname(abspath(__file__))
         css_file = join(module_dir, styles_filename)
@@ -74,85 +96,101 @@ class RestructuredtextHtmlPanel(Gtk.ScrolledWindow):
         self.view.show()
 
     def update_view(self, parent_window):
-    
-        if self.last_position_active:
-            print("saving scroll position")
-            self.view.run_javascript("[window.scrollX, window.scrollY]", None, self.update_view_continue, parent_window)
-        else:
-            self.update_view_continue(None, None, parent_window)
+        if self.state == State.EXIT:
+            debug("EXIT")
+            return
 
-    def update_view_continue(self, _view, result, parent_window):    
         view = parent_window.get_active_view()
         language = None
         if view:
-            source_language = \
-                parent_window.get_active_document().get_language()
+            source_language = parent_window.get_active_document().get_language()
             if source_language:
                 language = source_language.get_name()
-
-        try:
-            if result:
-                self.last_position = self.view.run_javascript_finish(result).get_js_value().to_string()
-        except GLib.Error:
-            pass
-        print("last position set:", self.last_position)
 
         if language == 'reStructuredText':
             doc = view.get_buffer()
             if doc.get_selection_bounds():
+                self.state = State.SELECTION
+                debug("state = SELECTION")
                 start = doc.get_iter_at_mark(doc.get_insert())
                 end = doc.get_iter_at_mark(doc.get_selection_bound())
-                self.last_position_active = False
             else:
+                self.state = State.REST
+                debug("state = REST")
                 start = doc.get_start_iter()
                 end = doc.get_end_iter()
-                self.last_position_active = True
 
             text = doc.get_text(start, end, False)
 
-            self.show_rest = True
             with self.lock:
-                self.parent_window = parent_window
-                self.text = text
-            # Only start the background thread once there is idle time, otherwise it can hold the GIL for too long, blocking the editor.
+                self.preview_args = PreviewArgs(text=text, html_type=self.state, parent_window=parent_window)
+            # Only start the background thread once there is idle time, otherwise it can hold the GIL for too long,
+            # blocking the editor.
             GLib.idle_add(self.set_event)
         else:
-            self.show_rest = False
-            self.last_position_active = False
+            self.state = State.NON_REST
+            debug("state = NON_REST")
             html = '<h3>reStructuredText Preview</h3>\n' \
                    '<p>' \
                    '<em>Switch file language to</em> reStructuredText ' \
                    '<em>to render the document</em>' \
                    '</p>'
-            base_uri = ''
 
-            self.view.load_html(self.TEMPLATE.format(
-                body=html, css=self.styles, scripts=''
-            ), base_uri)
+            self.save_scroll_position(PreviewArgs(html=html, html_type=State.NON_REST, parent_window=parent_window))
 
     def set_event(self):
         self.event.set()
         return False
 
-    def update_callback(self, html, parent_window):
-        if not self.show_rest:
+    def save_scroll_position(self, args):
+        if self.state == State.EXIT:
             return False
 
-        location = parent_window.get_active_document().get_location()
-        base_uri = location.get_uri() if location else ''
-        script = ''
-        if self.last_position_active and self.last_position:
-            print("restoring position:", self.last_position)
-            script = f"<script>window.scroll({self.last_position})</script>\n"
-        document = self.TEMPLATE.format(body=html, css=self.styles, scripts=script)
-
-        self.view.load_html(document, base_uri)
-
+        if self.scroll_position_valid:
+            debug("saving scroll position")
+            self.view.run_javascript("[window.scrollX, window.scrollY]",
+                            None, self.display_html, args)
+        else:
+            self.display_html(None, None, args)
         return False  # stop idle_add from calling us again
 
+    def display_html(self, _view, scrollresult, args):
+        if self.state == State.EXIT:
+            return
+
+        if scrollresult:
+            try:
+                self.last_position = self.view.run_javascript_finish(scrollresult) \
+                                        .get_js_value().to_string()
+                debug("last_position = ", self.last_position)
+            except GLib.Error as e:
+                print("Error retrieving reST preview scroll position:", e,
+                      file=sys.stderr)
+
+        if args.html_type != self.state:
+            debug("stale callback", self.state, ", not rendering html")
+            return
+
+        base_uri = ''
+        if self.state in [State.REST, State.SELECTION]:
+            location = args.parent_window.get_active_document().get_location()
+            base_uri = location.get_uri() if location else ''
+
+        script = ''
+        debug("last_position:", self.last_position)
+        if self.state == State.REST and self.last_position:
+            script = f"<script>window.scroll({self.last_position})</script>\n"
+            debug("restoring position in new html")
+        document = self.TEMPLATE.format(body=args.html, css=self.styles, scripts=script)
+
+        debug("Rendering", self.state, "html")
+        self.view.load_html(document, base_uri)
+        self.scroll_position_valid = (self.state == State.REST)
+        debug("scroll_position_valid =", self.scroll_position_valid)
+
     def clear_view(self):
-        self.alive = False
-        self.show_rest = False
+        self.state = State.EXIT
+        debug("state = EXIT")
         self.event.set()
         self.view.load_html('', '')
 
@@ -161,16 +199,17 @@ class RestructuredtextHtmlPanel(Gtk.ScrolledWindow):
             self.event.wait()  # Block until there's something to do
             self.event.clear()
 
-            if self.alive == False:
+            if self.state == State.EXIT:
                 return
 
             with self.lock:
-                parent_window, text = self.parent_window, self.text
-                self.parent_window, self.text = None, None
-            if not text:
+                args = self.preview_args
+                self.preview_args = None
+            if not args:
                 continue
 
-            html = publish_parts(text, writer_name='html')['html_body']
+            args.html = publish_parts(args.text, writer_name='html')['html_body']
+            args.text = None
             # We're not allowed to call Gtk methods on this thread
-            GLib.idle_add(self.update_callback, html, parent_window)
+            GLib.idle_add(self.save_scroll_position, args)
 
