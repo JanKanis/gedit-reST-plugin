525bc4ce46ec714fd26eee09bb2857a8b394d49c
Peter Bittner <django@bittner.it>
2020-10-28 00:34:26 +0100

    Add settings schema, persist preferences value

--- reST/config.py
+++ reST/config.py
@@ -18,38 +18,53 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import os
+
 from gi.repository import Gio, Gtk
 
-__all__ = ['RestructuredtextConfigWidget']
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+REST_KEY_BASE = 'org.gnome.gedit.plugins.restructuredtext'
+REST_KEY_PREVIEW_PANEL = 'preview-panel'
+REST_PREVIEW_PANELS = ['bottom-panel', 'side-panel']
+
+
+def get_settings():
+    """
+    Read the compiled settings schema from disk.
+    """
+    schemas_path = os.path.join(BASE_PATH, 'schemas')
+    schema_source = Gio.SettingsSchemaSource.new_from_directory(
+        schemas_path, Gio.SettingsSchemaSource.get_default(), False)
+    schema = schema_source.lookup(REST_KEY_BASE, False)
+
+    settings = Gio.Settings.new_full(schema, None, None)
+    return settings
 
 
 class RestructuredtextConfigWidget(object):
 
-    REST_KEY_BASE = 'org.gnome.gedit.plugins.restructuredtext'
-    REST_KEY_PREVIEW_PANEL = 'preview-panel'
-
     def __init__(self, datadir):
-        object.__init__(self)
+        super().__init__()
 
         self._ui_path = os.path.join(datadir, 'config.ui')
-        # self._settings = Gio.Settings.new(self.REST_KEY_BASE)
+        self._settings = get_settings()
         self._ui = Gtk.Builder()
 
     def configure_widget(self):
         self._ui.add_from_file(self._ui_path)
 
-        for panel_id in ['bottom-panel', 'side-panel']:
-            active = True  # self._settings.get_string(self.REST_KEY_PREVIEW_PANEL) == panel_id
+        preview_panel = self._settings.get_int(REST_KEY_PREVIEW_PANEL)
+
+        for index, panel_id in enumerate(REST_PREVIEW_PANELS):
             radiobutton = self._ui.get_object(panel_id)
             radiobutton.connect('toggled', self.on_button_toggled, panel_id)
-            radiobutton.set_active(active)
+            radiobutton.set_active(preview_panel == index)
 
         widget = self._ui.get_object('restructuredtext_preferences')
         return widget
 
     def on_button_toggled(self, radiobutton, panel_id):
         if radiobutton.get_active():
-            # self._settings.set_string(self.REST_KEY_PREVIEW_PANEL, panel_id)
-            print(panel_id)
+            index = REST_PREVIEW_PANELS.index(panel_id)
+            self._settings.set_int(REST_KEY_PREVIEW_PANEL, index)
 
 # ex:et:ts=4:
